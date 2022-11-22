19e86775d59732c5ee6ed47d961660e4efab4ec6
Lennox Apudo <80188775+LennoxFroide@users.noreply.github.com>
2022-11-21 19:04:36 -0500

    Imported django_on_heroku instead of django_heroku

--- taskproject/settings.py
+++ taskproject/settings.py
@@ -1,7 +1,7 @@
 
 import os
 import environ
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,4 +130,4 @@ LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
 
 # Heroku server code
-django_heroku.settings(locals())
+django_on_heroku.settings(locals() )
